9eac6b4d58f8873a821673594af2d0e3dba91bff
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2023-09-20 00:42:43 +0300

    implement Navigation + Add productViewModel + Add ItemDetailsViewController + Add ItemsDetailsViewModel

---
+++ PizzaWorld/UI/ItemDetails/ItemDetailsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ItemDetailsViewController.swift
+//  PizzaWorld
+//
+//  Created by Omar on 19/09/2023.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+class ItemDetailsViewController: BaseViewController<ItemDetailsViewModel> {
+
+    @IBOutlet weak var ratingView: RatingView!
+    
+    
+    @IBOutlet weak var pizzaTitle: UILabel!
+    
+    
+    @IBOutlet weak var describtion: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        viewModel.viewDidLoad()
+    }
+
+    override func bind(viewModel: ItemDetailsViewModel) {
+        viewModel.displayMainData.subscribe(onNext:{ [weak self]productViewModel in
+          
+                self?.pizzaTitle.text = productViewModel.title
+            self?.ratingView.configureWithRating(rating: productViewModel.rating)
+                self?.ratingView.rate.text = "\(productViewModel.rating)"
+            self?.describtion.text = productViewModel.desc
+            
+        }).disposed(by: bag)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PizzaWorld/Utility/Protocols/Navigator.swift
+++ PizzaWorld/Utility/Protocols/Navigator.swift
@@ -8,17 +8,31 @@
 import Foundation
 import UIKit
 
+
+enum NavigationType{
+    case push
+    case present
+    case root
+}
 protocol Navigator{
     associatedtype Destination
     var coordinator : Coordinator { get}
     init(coordinaor : Coordinator)
     
     func viewController(for destenation:Destination)->UIViewController
-    func navigate(to destination: Destination)
+    func navigate(to destination: Destination , navigationType : NavigationType)
 }
 extension Navigator{
-    func navigate(to destination : Destination){
+    func navigate(to destination : Destination, navigationType : NavigationType){
         let view = viewController(for: destination)
-        coordinator.navigationController?.pushViewController(view, animated: true)
+        switch navigationType{
+        case .push:
+            coordinator.navigationController?.pushViewController(view, animated: true)
+        case .present:
+            coordinator.navigationController?.present(view, animated: true)
+        case .root:
+            coordinator.navigationController?.setViewControllers([view], animated: true)
+        }
+        
     }
 }
